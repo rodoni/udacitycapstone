db7e5afb69cf8b8942f690f5c70205f4e7edf61b
rodoni <rafael.odoni@gmail.com>
2017-05-03 18:45:00 -0300

    type: feat
    Start a creation of Neural Network arquiteture and train loop
    
    Reference Udacity capstone work

---
+++ neural_network.py
@@ -0,0 +1,8 @@
+import input_files as input_file
+import tensorflow as tf
+
+class NeuralNetworkLSTM(object):
+    """
+    A LSTM based in a deep siamese neural network for text similarity
+    """
+
--- train_neural_network.py
+++ train_neural_network.py
@@ -2,3 +2,5 @@ import input_files as input_file
 import tensorflow as tf
 
 
+
+
