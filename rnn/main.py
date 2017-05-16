f25ab4b1015e903a3be9bff3401463733e861e5a
rodoni <rafael.odoni@gmail.com>
2017-05-16 08:15:58 -0300

    doc: Modify project structure
    Was modify the project to comport other examples.

---
+++ rnn/main.py
@@ -0,0 +1,12 @@
+import input_files as input_file
+import train_neural_network as train_nn
+import numpy as np
+
+
+data_questions = input_file.InputHelper('/home/rodoni/DataSet/Quora/test.csv',
+                              '/home/rodoni/DataSet/Quora/train.csv')
+
+questions_1, questions_2, is_duplicated, vocab_size, max_document_size = data_questions.get_train_data()
+
+rnn = train_nn.TrainNeuralNetworkSiamese(max_document_size, questions_1, questions_2, is_duplicated, vocab_size)
+rnn.train_nn()
